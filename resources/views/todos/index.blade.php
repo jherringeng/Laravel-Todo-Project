33522ec426e1591b2c4f2d58a69af0bbb4c009a7
dirtyCount <dirtyCount@gmail.com>
2019-10-06 17:50:24 +0100

    Added status and logic for Status on View Todos

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -23,10 +23,40 @@
 
           {{ $todo->name }}
 
-          <a href="/delete-todos/{{  $todo->id  }}" class="btn btn-danger sm float-right">Delete</a>
-          <a href="/edit-todos/{{  $todo->id  }}" class="btn btn-warning sm float-right">Edit</a>
-          <a href="/todos/{{  $todo->id  }}" class="btn btn-success sm float-right">View</a>
-          <button type="button" data-toggle="collapse" data-target="#see-todo-desc-{{ $todo->id }}" class="btn btn-primary sm float-right seeMore">Details</button>
+          <a href="/delete-todos/{{  $todo->id  }}" class="btn btn-danger sm ml-1 float-right">Delete</a>
+          <a href="/edit-todos/{{  $todo->id  }}" class="btn btn-warning sm ml-1 float-right">Edit</a>
+          <a href="/todos/{{  $todo->id  }}" class="btn btn-success sm ml-1 float-right">View</a>
+          <button type="button" data-toggle="collapse" data-target="#see-todo-desc-{{ $todo->id }}" class="btn btn-primary sm ml-1 float-right seeMore">Details</button>
+          <div id="priority-status-{{ $todo->id }}" class="btn col-sm-1 ml-1 mr-4 float-right">Unknown</div>
+          <div class="btn col-sm-1 ml-1 float-right">Status:</div>
+          <script>
+
+            var statusComplete = "{{ $todo->completed }}";
+            var statusPriority = "{{ $todo->priority }}";
+
+            if (statusComplete == 1) {
+
+              $("#priority-status-{{ $todo->id }}").text("Complete");
+              $("#priority-status-{{ $todo->id }}").addClass("btn-primary");
+
+            } else if ((statusPriority == "Low")){
+
+              $("#priority-status-{{ $todo->id }}").text("Low");
+              $("#priority-status-{{ $todo->id }}").addClass("btn-success");
+
+            } else if ((statusPriority == "Medium")){
+
+              $("#priority-status-{{ $todo->id }}").text("Medium");
+              $("#priority-status-{{ $todo->id }}").addClass("btn-warning");
+
+            } else if ((statusPriority == "High")){
+
+              $("#priority-status-{{ $todo->id }}").text("High");
+              $("#priority-status-{{ $todo->id }}").addClass("btn-danger");
+
+            }
+
+          </script>
 
           <ul class="list-group">
               <li class="list-group-item collapse" id = "see-todo-desc-{{ $todo->id }}">{{ $todo->description }}</li>
