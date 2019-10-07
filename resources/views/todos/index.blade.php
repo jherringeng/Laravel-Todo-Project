3ca77ceba5e552754ecf52f9b9cd28b19523cb2e
dirtyCount <dirtyCount@gmail.com>
2019-10-07 15:21:42 +0100

    Added Delete modal to View Todos and created file for @include in components

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -36,7 +36,10 @@
 
           {{ $todo->name }}
 
-          <a href="/delete-todos/{{  $todo->id  }}" class="btn btn-danger sm ml-1 float-right">Delete</a>
+          <button type="button" class="btn btn-danger sm ml-1 float-right" data-toggle="modal" data-target="#deleteModal" data-name="{{ $todo->name }}" data-link="/delete-todos/{{  $todo->id  }}">
+            Delete
+          </button>
+
           <a href="/edit-todos/{{  $todo->id  }}" class="btn btn-warning sm ml-1 float-right">Edit</a>
           <a href="/todos/{{  $todo->id  }}" class="btn btn-success sm ml-1 float-right">View</a>
           <button type="button" data-toggle="collapse" data-target="#see-todo-desc-{{ $todo->id }}" class="btn btn-primary sm ml-1 float-right seeMore">Details</button>
@@ -81,5 +84,41 @@
 
     </ul>
   </div>
+
+    <!-- Modal -->
+  <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="deleteModalLabel">Delete Todo</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body" id="deleteTodoText">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+          <a id = "deleteTodoLink" class="btn btn-primary" href = "https://www.jherring.tk/2019/07/20/project-control/">Delete</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script>
+
+    $('#deleteModal').on('show.bs.modal', function (event) {
+
+          var button = $(event.relatedTarget);
+          var name = button.data('name');
+          var link = button.data('link');
+          var modal = $(this)
+          modal.find('#deleteTodoText').html(name);
+          $("#deleteTodoLink").attr("href", link);
+
+        });
+
+    </script>
 
 @endsection
